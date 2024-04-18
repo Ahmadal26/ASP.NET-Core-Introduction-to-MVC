f5d5a5cb4485e3663ad4d3c3308c981ff25a3ccb
KFH <KFH@LAPTOP-TRN-145>
2024-04-18 13:54:49 +0300

    Add project files.

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<Customer> customers;
+
+        public CustomerController()
+        {
+          
+            customers = new List<Customer>
+            {
+                new Customer { CivilId = "298060590995", Name = "Ahmad", Email = "87517@gmail.com", Phone = 99840840 },
+                new Customer { CivilId = "308060500495", Name = "Osama", Email = "42131@gmail.com", Phone = 99587841 },
+                new Customer { CivilId = "300260500495", Name = "Abdulwahab", Email = "24131@gmail.com", Phone = 96466009 }
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public IActionResult Details(string civilId)
+        {
+            var customer = customers.Find(c => c.CivilId == civilId);
+            if (customer == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+    }
+}
---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿public class Customer
+{
+    public string CivilId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int Phone { get; set; }
+}
+
